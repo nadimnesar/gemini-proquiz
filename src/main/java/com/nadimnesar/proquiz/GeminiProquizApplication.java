4d1613a58447d98bc984d32d1cc1de32d44f64fe
nadimnesar <nadimnesar.cse@gmail.com>
2024-07-02 21:02:29 +0600

    project setup

---
+++ src/main/java/com/nadimnesar/proquiz/GeminiProquizApplication.java
@@ -0,0 +1,13 @@
+package com.nadimnesar.proquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiProquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiProquizApplication.class, args);
+	}
+
+}
